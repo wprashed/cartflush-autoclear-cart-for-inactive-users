f40b2de651991ce625b0b3b603f37457ee40a723
Rashed Hossain <wprashed@icloud.com>
2026-03-29 21:25:00 +0600

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,19 @@
 ( function() {
+	function getNextIndex( target ) {
+		let maxIndex = -1;
+
+		Array.from( target.querySelectorAll( 'select, input' ) ).forEach( function( field ) {
+			const name = field.getAttribute( 'name' ) || '';
+			const match = name.match( /\[(\d+)\]\[[^\]]+\]$/ );
+
+			if ( match ) {
+				maxIndex = Math.max( maxIndex, parseInt( match[1], 10 ) );
+			}
+		} );
+
+		return maxIndex + 1;
+	}
+
 	function addRow(event) {
 		const button = event.currentTarget;
 		const type = button.getAttribute( 'data-cartflush-add-row' );
@@ -14,7 +29,7 @@
 			return;
 		}
 
-		const index = target.children.length;
+		const index = getNextIndex( target );
 		const html = template.innerHTML.replace( /{{index}}/g, index );
 
 		target.insertAdjacentHTML( 'beforeend', html );
